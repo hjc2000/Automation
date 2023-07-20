22cb92b876d92a7e7af9b997f25e4f9a7ad9bd86
黄锦灿 <hjc.666@foxmail.com>
2023-07-20 22:17:11 +0800

    startup.bat

--- StartUp/Program.cs
+++ StartUp/Program.cs
@@ -1,34 +1,23 @@
 ﻿using AutomationLib;
 
 using CMD cmd = new();
-string[] cmdStrs = new[]
+using FileStream cmdFile = File.Open("./cmd.txt", FileMode.Open, FileAccess.Read, FileShare.Read);
+using StreamReader sr = new(cmdFile);
+while (true)
 {
-	"\"C:\\Program Files\\LGHUB\\lghub.exe\"",
-};
-
-foreach (string cmdStr in cmdStrs)
-{
-	Console.WriteLine($"执行：{cmdStr}");
-	Console.WriteLine(await cmd.RunCommandAsync(cmdStr));
+	string? cmdStr = await sr.ReadLineAsync();
+	if (cmdStr == null)
+	{
+		break;
+	}
+	else
+	{
+		cmdStr = cmdStr.Trim();
+		// 空命令不执行
+		if (cmdStr != string.Empty)
+		{
+			Console.WriteLine($"执行：{cmdStr}");
+			Console.WriteLine(await cmd.RunCommandAsync(cmdStr));
+		}
+	}
 }
-
-//using FileStream cmdFile = File.Open("./cmd.bat", FileMode.Open, FileAccess.Read, FileShare.Read);
-//using StreamReader sr = new(cmdFile);
-//while (true)
-//{
-//	string? cmdStr = await sr.ReadLineAsync();
-//	if (cmdStr == null)
-//	{
-//		break;
-//	}
-//	else
-//	{
-//		cmdStr = cmdStr.Trim();
-//		// 空命令不执行
-//		if (cmdStr != string.Empty)
-//		{
-//			Console.WriteLine(cmdStr);
-//			Console.WriteLine(await cmd.RunCommandAsync(cmdStr));
-//		}
-//	}
-//}
---
+++ StartUp/startup.bat
@@ -0,0 +1,3 @@
+rem 将此文件放到开机启动目录
+"C:\我的可执行文件\StartUp\StartUp.exe"
+exit
