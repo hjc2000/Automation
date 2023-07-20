d65d6fcfaff8fd3f378dedeb9860710f58900ecc
hjc2000 <hjc.666@foxmail.com>
2023-07-20 11:25:51 +0800

    1

---
+++ MountNFS/Program.cs
@@ -0,0 +1,14 @@
+﻿using AutomationLib;
+
+#region 挂载
+MountNFS mount = new();
+string[] hosts = new string[]
+{
+	"192.168.8.8",
+};
+
+await mount.Mount(hosts);
+#endregion
+
+// 让主线程不要退出
+Console.ReadLine();
