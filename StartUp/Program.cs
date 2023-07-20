4871ae9043c67374c0d52efa6d05eda95d6d2c18
黄锦灿 <hjc.666@foxmail.com>
2023-07-20 21:38:52 +0800

    1

---
+++ StartUp/Program.cs
@@ -0,0 +1,34 @@
+﻿using AutomationLib;
+
+using CMD cmd = new();
+string[] cmdStrs = new[]
+{
+	"\"C:\\Program Files\\LGHUB\\lghub.exe\"",
+};
+
+foreach (string cmdStr in cmdStrs)
+{
+	Console.WriteLine($"执行：{cmdStr}");
+	Console.WriteLine(await cmd.RunCommandAsync(cmdStr));
+}
+
+//using FileStream cmdFile = File.Open("./cmd.bat", FileMode.Open, FileAccess.Read, FileShare.Read);
+//using StreamReader sr = new(cmdFile);
+//while (true)
+//{
+//	string? cmdStr = await sr.ReadLineAsync();
+//	if (cmdStr == null)
+//	{
+//		break;
+//	}
+//	else
+//	{
+//		cmdStr = cmdStr.Trim();
+//		// 空命令不执行
+//		if (cmdStr != string.Empty)
+//		{
+//			Console.WriteLine(cmdStr);
+//			Console.WriteLine(await cmd.RunCommandAsync(cmdStr));
+//		}
+//	}
+//}
