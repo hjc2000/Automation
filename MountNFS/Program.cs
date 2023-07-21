30cfb182adcf776bc5cf29950e4db31c95c8a220
hjc2000 <hjc.666@foxmail.com>
2023-07-21 10:42:31 +0800

    1

--- MountNFS/Program.cs
+++ MountNFS/Program.cs
@@ -1,27 +1,5 @@
 ﻿using AutomationLib;
 
-using CMD cmd = new();
-using FileStream cmdFile = File.Open(@"C:\我的可执行文件\cmd.txt", FileMode.Open, FileAccess.Read, FileShare.Read);
-using StreamReader sr = new(cmdFile);
-while (true)
-{
-	string? cmdStr = await sr.ReadLineAsync();
-	if (cmdStr == null)
-	{
-		break;
-	}
-	else
-	{
-		cmdStr = cmdStr.Trim();
-		// 空命令不执行
-		if (cmdStr != string.Empty)
-		{
-			Console.WriteLine($"执行：{cmdStr}");
-			Console.WriteLine(await cmd.RunCommandAsync(cmdStr));
-		}
-	}
-}
-
 #region 挂载
 MountNFS mount = new();
 string[] hosts = new string[]
@@ -53,3 +31,4 @@ await mount.Mount(hosts);
 #endregion
 
 Console.WriteLine("完成");
+Console.ReadLine();
