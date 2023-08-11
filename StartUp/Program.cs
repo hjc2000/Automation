cda3a0873ae704687a2b2439b2df7f1a054e78aa
hjc2000 <hjc.666@foxmail.com>
2023-08-11 23:07:27 +0800

    用 System.Diagnostics.Process.GetCurrentProcess().MainModule?.FileName 获取可执行文件路径

--- StartUp/Program.cs
+++ StartUp/Program.cs
@@ -1,11 +1,9 @@
 ﻿using AutomationLib;
 
 using CMD cmd = new();
-/* 这里使用绝对路径来查找 StartUp.cmd 是因为相对路径 ./ 获得的路径会根据本程序被启动的位置变化
- * 而变化。例如在一个别的目录下用 shell 启动本程序，则 ./ 指向的是 shell 的工作目录，而不是本
- * 程序的 exe 文件所在的目录
- */
-using FileStream cmdFile = File.Open(@"C:\msys64\home\huang\my_app\StartUp.cmd", FileMode.Open, FileAccess.Read, FileShare.Read);
+string fullPath = System.Diagnostics.Process.GetCurrentProcess().MainModule?.FileName ?? string.Empty;
+string dir = Path.GetDirectoryName(fullPath) ?? string.Empty;
+using FileStream cmdFile = File.Open($"{dir}\\StartUp.cmd", FileMode.Open, FileAccess.Read, FileShare.Read);
 using StreamReader sr = new(cmdFile);
 
 try
